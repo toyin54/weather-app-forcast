17d6ce7fbbf9c4c6fbc06f817f395f024b0c1dd4
Ayyub Jose <ajose3@depaul.edu>
2024-02-26 13:52:06 -0500

    finalized navigation

--- src/components/SearchDialog/SearchBar.tsx
+++ src/components/SearchDialog/SearchBar.tsx
@@ -13,6 +13,17 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Github } from 'lucide-react'
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
 export default function SearchBar() {
   const [location, setLocation] = useState<string>('')
 
@@ -25,15 +36,24 @@ export default function SearchBar() {
         <DialogTrigger asChild>
           <Button
             variant="outline"
-            className='border inline-flex items-center justify-center text-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-slate-100 ease-in-out duration-200'
+            className='border inline-flex items-center justify-center text-center text-sm font-medium hover:dark:bg-[#131313] hover:bg-red-100 ease-in-out duration-200 dark:border-white border-black'
           >
             <p className='text-sm text-muted-foreground'>Search Here..</p>
-            <div className="command dark:bg-[#262626] bg-slate-200 py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
+            <div className="command dark:bg-[#262626] bg-slate-200 dark:border-white border-black py-[2px] pl-[5px] pr-[7px] rounded-sm ml-[10rem] flex items-center gap-2">
               <Github />
               <span className='text-[9px]'>F</span>
             </div>
           </Button>
         </DialogTrigger>
+        <DialogContent className= 'p-0'>
+            <Command className='rounded-lg border shadow-white '>
+             <CommandInput 
+             placeholder='Type in a command or search ...'/>
+             <ul className='px-3 pb-2'>
+              <p className='p-2 text-sm text-muted-foreground'>Suggestions</p>
+             </ul>
+            </Command>
+          </DialogContent>
       </Dialog>
     </div>
   )
